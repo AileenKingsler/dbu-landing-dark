4d09f4b524c06e73f90046c775a2d9a67d625388
Алёна Царегородцева <aileen.kingsler@mail.ru>
2022-06-06 16:36:41 +0400

    clients and works

--- gulpfile.js
+++ gulpfile.js
@@ -55,6 +55,7 @@ gulp.task("scripts", function () {
     gulp.src([
       "node_modules/jquery/dist/jquery.min.js",
       "node_modules/bootstrap/dist/js/bootstrap.bundle.min.js",
+      "source/js/jquery.fancybox.min.js",
       "source/js/scripts.js"
       ]),
     concat("scripts.min.js"),
