765c45f0c263599b7538bcadd39dcb2e059e5ddd
Алёна Царегородцева <aileen.kingsler@mail.ru>
2022-06-07 15:03:45 +0400

    mail

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,3 +1,40 @@
 $(function () {
 
+  $('.form').submit(function () {
+    var th = $(this);
+
+    $.ajax({
+      type: 'POST',
+      url: 'mail.php',
+      data: th.serialize()
+    }).done(function () {
+      th.trigger('reset');
+      $('#successModal').modal('show');
+      setTimeout(function () {
+        $('#successModal').modal('hide');
+      }, 3000);
+    });
+
+    return false;
+  });
+
+  $('.modal-form').submit(function () {
+    var th = $(this);
+
+    $.ajax({
+      type: 'POST',
+      url: 'mail.php',
+      data: th.serialize()
+    }).done(function () {
+      th.trigger('reset');
+      th.find('.send-success').show();
+      setTimeout(function () {
+        $('.modal').modal('hide');
+        th.find('.send-success').hide();
+      }, 3000);
+    });
+
+    return false;
+  });
+
 });
