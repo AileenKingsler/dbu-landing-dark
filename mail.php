be27863fb30ea48846c8cc52a96b6ddf55b6aa6f
Алёна Царегородцева <aileen.kingsler@mail.ru>
2022-06-09 20:42:58 +0400

    Updates

---
+++ mail.php
@@ -0,0 +1,18 @@
+<?php
+
+$address = "ikramova999@mail.ru";
+$subject = "Сообщение с сайта ДБУ";
+
+$name = htmlspecialchars($_POST["name"]);
+$phone = htmlspecialchars($_POST["phone"]);
+$question = htmlspecialchars($_POST["question"]);
+
+$mess = "Телефон: $phone";
+if ($name) {
+  $mess .= "\r\nИмя: $name";
+}
+if ($question) {
+  $mess .= "\r\nВопрос: $question";
+}
+
+mail($address, $subject, $mess);
